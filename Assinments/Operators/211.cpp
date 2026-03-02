972f18f3b386ce97402763b72e44fcff5519433b
Anas Ahmad <anas.ahmad.mohamed@gmail.com>
2026-03-02 12:18:05 +0200

    Add files via upload

---
+++ Assinments/Operators/211.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Bello\b\b\b\b\bHello Elzeroo\b";
+  cout << " Web Schoolll\b\b";
+  cout << " I Love Programming";  // ass
+  cout << " Too Much";            // ans
+  cout << " Specially C++";
+  return 0;
+}
+
+// Needed Output
+// "Hello Elzero Web School I Love Programming Too Much Specially C++"
+
+// '/'
---
+++ Assinments/Operators/405.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Assign The Values
+  char a = '1';
+  char b = '1';
+  char c = '1';
+  
+  a = 100;
+  b = 10;
+  c = 20;
+  
+  // Do Not Edit
+  cout << b * c + a << "\n"; // 1000
+  
+  return 0;
+}
