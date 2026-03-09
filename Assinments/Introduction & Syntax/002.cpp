ebb2abd12361614b9a950906e0fe3b264c59428c
Anas Ahmad <anas.ahmad.mohamed@gmail.com>
2026-03-09 12:03:16 +0200

    Delete Assinments directory

--- Assinments/Introduction & Syntax/002.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    cout << "Iam Learning Programming\n" << "With\n" << "Elzero\n" << "Web\n" << "School";
-    return 0
-}
-
--- Assinments/Operators/202.2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    cout << 10 * 20 * (15 / 3) - 190 + 10 - 400 << "\n"; // 0 I Don't Know
-    return 0;
-}
